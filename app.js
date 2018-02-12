8ed903cca8c7d1bf5ff426e4a59451b3082c52a7
Daniel Lindskoge <daniel@waymoot.se>
2018-02-12 17:14:04 +0100

    end of Section 4, Lecture 17

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
 const app = express();
+
+// map global promise - get rid of warning (not necessary on this version of mongoose)
+// mongoose.Promise = global.Promise
+// connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev')  // useMongoDBClient is no longer necessary
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+// Load Idea model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
 
 // Add Handlebars as Template engine to express
 app.engine('handlebars', exphbs({
@@ -20,6 +33,11 @@ app.get('/', (req, res) => {
 // About Route
 app.get('/about', (req, res) => {
   res.render('about');
+});
+
+// Add Idea form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
 });
 
 // starting the express erver

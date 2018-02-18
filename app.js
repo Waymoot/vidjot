f1178956fe99d3860995fa6b8f91d57ca121b708
Daniel Lindskoge <daniel@waymoot.se>
2018-02-18 14:49:48 +0100

    We can now login and updated the links in navbar

--- app.js
+++ app.js
@@ -51,6 +51,10 @@ app.use(session({
   saveUninitialized: true
 }));
 
+// Passport session middleware (has to be after the session middleware)
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
 // Global variables
@@ -58,6 +62,7 @@ app.use(function(req, res, next){
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;   // user object (so we know the user is logged in)
   next();
 });
 
